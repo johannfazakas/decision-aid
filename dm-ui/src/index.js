7f7702bada9911d23c3dff7918d01e12feb4cdab
Johann Fazakas <johannfazakas@gmail.com>
2021-04-10 18:02:21 +0300

    init create-react-app as decision maker future ui

---
+++ dm-ui/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <h1>Decision Maker App! ... coming soon</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
